303462c2a7cd0819be456597d80bcb54d25f402c
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-10-03 14:29:05 +0200

    added reducer function for managing questions state and status of app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-
+import { useEffect, useReducer } from 'react'
 import Header from "./Header"
 import Main from "./Main"
+
+const intialState = {
+  questions:[],
+
+  //loading active error ready finished 
+  status: 'loading'
+}
+
+
+function reducer(state,action){
+
+
+  switch(action.type){
+      case "dataRecieved":
+          return {...state,questions:action.payload,status:"ready"}
+      case "dataFailed":
+          return {...state,status:"error"}    
+      default:
+        throw new Error("unknown case")
+
+}
+}
+
+
+
+
 export default function App(){
+  const [state,dispatch] = useReducer(reducer,intialState)
+
+
+  useEffect(function(){
+      async function fetchQuestions(){
+        try {
+          const response = await fetch("http://localhost:8000/questions")
+          const questions = await response.json()
+          dispatch({type:"dataRecieved",payload:questions})
+        } 
+        catch (error) {
+          dispatch({type:"dataFailed"})
+        }
+
+      }
+      fetchQuestions()
+  },[])
+
+
+
 return(
     <div className="app">
       <Header/>
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
+
 function Main() {
+
   return (
     <main className="main">
     <p>1/15</p>
