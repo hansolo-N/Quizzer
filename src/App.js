6dd7c9862079c76b90d51488c7dfa87fd6dbc043
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-10-03 13:11:07 +0200

    defined Main component for questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
-import DateCounter from "./DateCounter"
-
-
+import Header from "./Header"
+import Main from "./Main"
 export default function App(){
-return <div>
-  <DateCounter/>
-</div>
+return(
+    <div className="app">
+      <Header/>
+      <Main/>
+    </div>
+  ) 
+
 }
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <main className="main">
+    <p>1/15</p>
+    <p>question</p>
+  </main>
+  )
+}
+
+export default Main
